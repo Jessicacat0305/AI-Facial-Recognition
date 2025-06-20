6a6d1587f5f6e24c3b3975b00b87bb606e285602
Jessicacat0305 <jessicamak48@gmail.com>
2025-06-20 15:07:43 +0000

    test1

--- headshots.py
+++ headshots.py
@@ -1,33 +1,44 @@
+from picamera2 import Picamera2, Preview
 import cv2
+import os
 
-name = 'Caroline' #replace with your name
+name = 'jessica'
+save_dir = os.path.join("dataset", name)
+os.makedirs(save_dir, exist_ok=True)
 
-cam = cv2.VideoCapture(0)
+# Initialize the Picamera2 object
+picam2 = Picamera2()
 
-cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("press space to take a photo", 500, 300)
+# Configure the camera preview and set format and resolution
+config = picam2.create_still_configuration(main={"format": "RGB888", "size": (640, 480)})
+picam2.configure(config)
+
+# Start the camera preview
+picam2.start()
+
+cv2.namedWindow("Press space to take a photo", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Press space to take a photo", 500, 300)
 
 img_counter = 0
 
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("press space to take a photo", frame)
+    # Capture a frame from the camera
+    frame = picam2.capture_array()
+
+    # Show the frame
+    cv2.imshow("Press space to take a photo", frame)
 
     k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
+    if k % 256 == 27:
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+    elif k % 256 == 32:
+        # SPACE pressed, save the image
+        img_name = os.path.join(save_dir, f"image_{img_counter}.jpg")
         cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+        print(f"{img_name} written!")
         img_counter += 1
 
-cam.release()
-
+# Clean up and release the camera
 cv2.destroyAllWindows()
+picam2.stop()
--- headshots_picam.py
+++ headshots_picam.py
@@ -1,35 +1,44 @@
 import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from picamera2 import Picamera2
+import os
+import time
 
-name = 'Caroline' #replace with your name
+name = 'Jessuca'  # Replace with your name
 
-cam = PiCamera()
-cam.resolution = (512, 304)
-cam.framerate = 10
-rawCapture = PiRGBArray(cam, size=(512, 304))
-    
+# Create dataset directory if it doesn't exist
+save_dir = f"dataset/{name}"
+os.makedirs(save_dir, exist_ok=True)
+
+# Initialize PiCamera2
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (512, 304)})
+picam2.configure(config)
+picam2.start()
+
+# Allow camera to warm up
+time.sleep(2)
+
 img_counter = 0
 
 while True:
-    for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        cv2.imshow("Press Space to take a photo", image)
-        rawCapture.truncate(0)
+    # Capture frame as numpy array
+    frame = picam2.capture_array()
     
-        k = cv2.waitKey(1)
-        rawCapture.truncate(0)
-        if k%256 == 27: # ESC pressed
-            break
-        elif k%256 == 32:
-            # SPACE pressed
-            img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
-            cv2.imwrite(img_name, image)
-            print("{} written!".format(img_name))
-            img_counter += 1
-            
-    if k%256 == 27:
+    # Show the image in a window
+    cv2.imshow("Press Space to take a photo", frame)
+
+    # Wait for key press
+    k = cv2.waitKey(1)
+    
+    if k % 256 == 27:
+        # ESC pressed
         print("Escape hit, closing...")
         break
+    elif k % 256 == 32:
+        # SPACE pressed
+        img_name = f"{save_dir}/image_{img_counter}.jpg"
+        cv2.imwrite(img_name, frame)
+        print(f"{img_name} written!")
+        img_counter += 1
 
 cv2.destroyAllWindows()
